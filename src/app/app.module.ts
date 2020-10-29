ae7b44c9f29960b562e7aa188eb5b0f213496da9
Abiram <ram.abiram21@gmail.com>
2020-10-29 10:27:32 +0530

    Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Angular Material
 import { MatInputModule } from '@angular/material/input';
-import {ReactiveFormsModule} from '@angular/forms';
-
-// 
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 
+import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
